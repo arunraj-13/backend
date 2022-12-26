a576b99f3b1af9eecd5dc660c27ea9112071a67a
arun <av01032001@gmail.com>
2022-12-26 19:51:06 +0530

    first commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import "./models/question.js"
-
+import cors from 'cors'
 const app = express()
 const PORT = process.env.PORT || 5000
 
 dotenv.config()
 
-// app.use(cors());
+app.use(cors())
 // const { createProxyMiddleware } = require('http-proxy-middleware');
 // app.use('/api', createProxyMiddleware({ 
 //     target: 'http://localhost:8080/', //original url
@@ -18,11 +18,11 @@ dotenv.config()
 //        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
 //     }
 // }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-  });
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+//   });
 
 
 app.listen(PORT,()=>{
